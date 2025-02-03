16b4b21279f4c52041458b40eeca95fda56130e4
An2nyT8 <anthony_thet@yahoo.de>
2025-02-03 22:34:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 // JavaScript
+var isChanged = false; // Flag, um den Zustand zu verfolgen
+
 document.getElementById("changeTextBtn").addEventListener("click", function() {
-    // Hole das Text-Element
     var textElement = document.getElementById("text");
 
-    // Überprüfe, ob der Text bereits geändert wurde
-    if (textElement.innerText === "Der Text hat sich geändert!") {
+    if (isChanged) {
         // Wenn der Text bereits geändert wurde, setze ihn zurück
         textElement.innerText = "Dies ist der ursprüngliche Text.";
     } else {
-        // Andernfalls ändere den Text
+        // Wenn der Text noch nicht geändert wurde, ändere ihn
         textElement.innerText = "Der Text hat sich geändert!";
     }
+
+    // Toggle den Zustand
+    isChanged = !isChanged;
 });
